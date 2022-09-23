b92b9b3167a21f3b9db08e12a54aa8847446de4c
sooryap91 <sooryap1991@gmail.com>
2022-09-23 23:38:23 +0530

    changes

--- login.js
+++ login.js
@@ -3,17 +3,16 @@ let password=document.getElementById("password");
 let error=document.getElementById("error");
 function validate()
 {
-    let regexp=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
-    let passw = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]{8,})$/
+    let regexp=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+    let passw = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$/;
 
-        
-
-    if(regexp.test(email.value)){
+        if(regexp.test(email.value)){
         error.innerHTML="Valid";
         error.style.color="green";
         return true;
     }
-    else if(passw.test(password.value)){
+
+   if(passw.test(password.value)){
         error.innerHTML="Valid";
         error.style.color="green";
         return true;
