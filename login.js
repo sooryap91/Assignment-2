9195e4ab1c6020f3d2cce4ff26f1924ab5efb287
sooryap91 <sooryap1991@gmail.com>
2022-09-24 06:33:13 +0530

    changes

--- login.js
+++ login.js
@@ -4,22 +4,23 @@ let error=document.getElementById("error");
 function validate()
 {
     let regexp=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
-    let passw = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$/;
+    var passw = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$/;
 
-        if(regexp.test(email.value)){
+        if(regexp.test(email.value) && (passw.test(password.value))) 
+        {
         error.innerHTML="Valid";
         error.style.color="green";
         return true;
-    }
-
-   if(passw.test(password.value)){
-        error.innerHTML="Valid";
-        error.style.color="green";
-        return true;
-    }
+        }
+    //    else if(passw.test(password.value))
+    // {
+    //     error.innerHTML="Valid";
+    //     error.style.color="green";
+    //     return true;
+    // }
     else{
         error.innerHTML="Invalid";
         error.style.color="red";
         return false;
-    }
+        }
 }
